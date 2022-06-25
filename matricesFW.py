702c75b816a0cd7c11c66878f3cf955ca61fc255
gabrielecarbone <74857588+gabrielecarbone@users.noreply.github.com>
2022-06-25 18:05:36 +0200

    Added Comments

--- matricesFW.py
+++ matricesFW.py
@@ -35,12 +35,13 @@ def alpha_binary_search(Zk, Dk, delta, max_value=1, min_value=0, tol=0.1):
         Increasing the tolerance means to speed up the algorithm and to raise the loss.
     
     INPUT:
-    Zk -- np. matrix of dimension [m,n] -- The current x
-    Dk -- np.matrix of dimension [m,n] -- The direction vector
+    Zk -- np. matrix of dimension [m, n] -- The current x
+    Dk -- np.matrix of dimension [m, n] -- The direction vector
     Delta -- integer -- The radius of the ball
     max_value -- float -- The maximum value of the search space
     min_value -- float -- The minimum value of the search space
-    tol -- float -- The tolerance of the algorihtm 
+    tol -- float -- The tolerance of the algorihtm
+    
     
     OUTPUT:
     best_alpha -- float -- The best alpha found
@@ -76,9 +77,9 @@ def compute_best_beta(Zk, X_rated, Dk, idx_rows, idx_cols, alpha_stop, stepsize 
     To increase/decrease the speed and accuracy of the algorithm it's possible to set different values for the stepsize and the tolerance.
     
     INPUT:
-    Zk -- np.matrix of dimension [m,n] -- The current x
+    Zk -- np.matrix of dimension [m, n] -- The current x
     X_rated -- np.array of dimension [p] -- The actual observed values
-    Dk -- np.matrix of dimension [m,n] -- The directions of the next iterate
+    Dk -- np.matrix of dimension [m, n] -- The directions of the next iterate
     idx_rows -- np.array of dimension [p] -- The row indices of the observed values. Used to find Z_rated.
     idx_cols -- np.array of dimension [p] -- The column indices of the observed values. Used to find Z_rated.
     alpha_stop -- float -- The alpha found in alpha_binary_search
@@ -114,16 +115,27 @@ def compute_best_beta(Zk, X_rated, Dk, idx_rows, idx_cols, alpha_stop, stepsize 
 
 # Regular FW algorithm
 def FrankWolfe(X, objective_function, delta, empties=0, printing = True, Z_init=None, max_iter=150, patience=1e-5):
-    '''
-    :param X: sparse matrix with ratings and 'empty values', rows - users, columns - books.
-    :param objective_function: objective function that we would like to minimize with FW
-    :param delta: Radius of the feasible's set ball
-    :param empties (optional): Empty values of X are zeros ('zeros') or NaN ('nan'). Default = 'zeros'.
-    :param Z_init (optional): In case we want to initialize Z with a known matrix- If not given, Z_init will be a zeros matrix. Default = None.
-    :param max_iter (optional): max number of iterations for the method. Default = 150.
-    :param patience (optional): once reached this tolerance provide the result. Default = 1e-5.
-    :return: Zk: matrix of predicted ratings - it should be like X but with no 'empty values'
-    :return: loss: objective function value with Zk
+    ''' PREDICTING VALUES USING THE REGULAR FRANK-WOLFE ALGORITHM
+    
+    DESCRIPTION:
+    The algorithm fill the matrix Zk based on some observed values (given by the sparse matrix X) while minimizing an objective function.
+    By changing the delta - the radius of the ball - it possible increase/decrease the accuracy.
+    
+    INPUT:
+    X -- np.matrix of dimension [m, n] -- sparse matrix with ratings and 'empty values', rows - users, columns - books.
+    objective_function -- function -- objective function that we would like to minimize with FW
+    delta -- float -- Radius of the feasible's set ball
+    [optional] empties -- string -- Empty values of X are zeros ('zeros') or NaN ('nan'). Default = 'zeros'.
+    [optional] Z_init -- np.matrix of dimension [m, n] In case we want to initialize Z with a known matrix- If not given, Z_init will be a zeros matrix. Default = None.
+    [optional] max_iter -- integer -- Max number of iterations for the method. Default = 150.
+    [optional] patience -- float -- Once reached this tolerance provide the result. Default = 1e-5.
+    
+    OUTPUT:
+    Zk -- np.matrix of dimension [m,n] -- Matrix of predicted ratings: it should be like X but with no 'empty values'
+    loss -- float -- The error associated with the final Zk
+    res_list -- list of lenght [iterations] -- A list containing the losses at each iteration
+    ranks -- list of lenght [iterations] -- A list containing the rank of each predicted Zk
+    
     '''
 
     # Get X indexes for not empty values
@@ -134,6 +146,7 @@ def FrankWolfe(X, objective_function, delta, empties=0, printing = True, Z_init=
     else:
         return print('Error: Empties argument', empties, 'not valid.')
 
+    # Separate the indexes coordinates
     idx_rows = idx_ratings[:, 0]
     idx_cols = idx_ratings[:, 1]
 
@@ -148,6 +161,8 @@ def FrankWolfe(X, objective_function, delta, empties=0, printing = True, Z_init=
     # Create vectors with the not empty features of the sparse matrix
     X_rated = X[idx_rows, idx_cols]
     Z_rated = Zk[idx_rows, idx_cols]
+    
+    # Compute the vector containing the differences between initial Zk matrix and the observed matrix X
     diff_vec = np.array(Z_rated - X_rated)[0]
 
     # Create needed variables
@@ -156,6 +171,7 @@ def FrankWolfe(X, objective_function, delta, empties=0, printing = True, Z_init=
     res_list = [loss]
     ranks = [np.linalg.matrix_rank(Zk)]
     it = 0
+    
     while (diff_loss > patience) and (it < max_iter):
 
         # Gradient
@@ -166,13 +182,12 @@ def FrankWolfe(X, objective_function, delta, empties=0, printing = True, Z_init=
 
         # Update
         Zk_tilde = -delta * np.outer(u_max, v_max)
-
-        alpha_k = 2 / (it + 2)  # alpha - as studied in class
+        alpha_k = 2 / (it + 2)  # alpha
         Zk = (1 - alpha_k) * Zk + alpha_k * Zk_tilde
 
         # Loss
-        diff_vec = np.array(Zk[idx_rows, idx_cols] - X_rated)[0]
-        new_loss = objective_function(diff_vec)
+        diff_vec = np.array(Zk[idx_rows, idx_cols] - X_rated)[0]  # compute the vector of differences between the predicted and the actual observed entries
+        new_loss = objective_function(diff_vec) # compute the loss of Zk
 
         # Improvement at this iteration
         diff_loss = np.abs(loss - new_loss)
@@ -186,6 +201,7 @@ def FrankWolfe(X, objective_function, delta, empties=0, printing = True, Z_init=
         # Count iteration
         it += 1
 
+        # Appending results
         res_list.append(loss)
         ranks.append(rankZ)
 
@@ -194,19 +210,36 @@ def FrankWolfe(X, objective_function, delta, empties=0, printing = True, Z_init=
 
 # FW extended in face
 def FW_inface(X, objective_function, delta, L = 1, D = None, gamma1 = 0, gamma2 = 1, empties = 0, max_iter=150, patience=1e-3, printing = True, delta_change = False):
-    '''
-    :param X: sparse matrix with ratings and 'empty values', rows - users, columns - books.
-    :param objective_function: objective function that we would like to minimize with FW.
-    :param delta: Radius of the feasible's set ball
-    :param L: must be greater than 1
-    :param D: if not inputed = 2*delta
-    :param gamma1: constraint on B step
-    :param gamma2: constraint on A step
-    :param empties (optional): Empty values of X are zeros (0) or NaN ('nan'). Default = 0
-    :param max_iter: max number of iterations for the method.
-    :param patience: once reached this tolerance provide the result.
-    :return: Z: matrix of predicted ratings - it should be like X but with no 'empty values'
-            loss: difference between original values (X) and predicted ones (Z).
+    ''' PREDICT VALUES USING IN-FACE FRANK-WOLFE
+    
+    DESCRIPTION:
+    The algorithm fill a sparse matrix by using the In-Face Frank-Wolfe method.
+    It's possible to control the algorithm performance by using several parameters.
+    Increasing gamma2 will lead to less iterations where the algorithm stay in the same face.
+    Increasing gamma1 will lead to less iterations where the algorithm go to a lower-dimensional face.
+    Changing delta influence the number of times the algorihtm will be inside or outside the ball.
+    Using the delta increaser will lead to more accurate, higher ranked matrices.
+    
+    INPUT:
+    X -- sparse matrix of dimension [m, n] -- A matrix with some observed values
+    objective_function -- function -- objective function that we would like to minimize with the In-Face FW
+    delta -- float -- Radius of the feasible's set ball
+    [optional] L -- float -- Lipshitz constant of the gradient of f(x). Default = 1
+    [optional] D -- float -- Diameter of the feasible set. Default = 2 * delta
+    [optional] gamma1 -- float -- Constraint on the B step. Must be 0 <= gamma1 <= gamma2. Default = 0
+    [optional] gamma2 -- float -- constraint on the A step. Must be 0 <= gamma2 <= 1. Default = 1
+    [optional] empties -- 0 or 'nan' -- Empty values of X are zeros (0) or NaN ('nan'). Default = 0
+    [optional] max_iter - integer -- Maximum number of iterations. Default = 150
+    [optional] patience -- float -- Once reached this tolerance provide the result. Default = 1e-3
+    [optional] printing -- bool -- Option to print the results while executing the algorithm. Default = True
+    [optional] delta_change -- bool -- Option to increase the delta at each iteration. Default = False
+    
+    OUTPUT:
+    Zk -- np.matrix of dimension [m,n] -- Matrix of predicted ratings: it should be like X but with no 'empty values'
+    loss -- float -- The error associated with the final Zk
+    res_list -- list of lenght [iterations] -- A list containing the losses at each iteration
+    ranks -- list of lenght [iterations] -- A list containing the rank of each predicted Zk
+    
     '''
 
     # Get X indexes for not empty values
@@ -216,7 +249,8 @@ def FW_inface(X, objective_function, delta, L = 1, D = None, gamma1 = 0, gamma2 
         idx_ratings = np.argwhere(~np.isnan(X))
     else:
         return print('Empties argument', empties, 'not valid.')
-
+    
+    # Separate the indexes coordinates
     idx_rows = idx_ratings[:,0]
     idx_cols = idx_ratings[:,1]
 
@@ -255,7 +289,8 @@ def FW_inface(X, objective_function, delta, L = 1, D = None, gamma1 = 0, gamma2 
     res_list = [objective_function(diff_vec_1), loss]
     ranks = [rank_Z]
     not_in_ball=False
-
+    
+    # Add counters
     B_used = 0
     A_used = 0
     not_entered = 0
@@ -277,9 +312,12 @@ def FW_inface(X, objective_function, delta, L = 1, D = None, gamma1 = 0, gamma2 
         grad = sparse.csr_matrix((diff_vec, (idx_rows, idx_cols)))
 
         # In-face direction with the away step strategy: two calculations depending of where Z lies within the feasible set
+        
         if np.isclose(s_thin.sum(), delta, rtol=1e-3)==True: # Z in border (sum of singular values == radius of feasible set)
+            
             in_border += 1
             G = 0.5 * (Vh_thin.dot(grad.T.dot(U_thin)) + U_thin.T.dot(grad.dot(Vh_thin.T)))
+            
             # Obtain unitary eigenvector corresponding to smallest eigenvalue of G
             eigvalues, eigvectors = np.linalg.eig(G)  #find the eigenvalues
             min_eig = np.argmin(eigvalues)  #find the index of the smallest eigenvalue
@@ -293,58 +331,77 @@ def FW_inface(X, objective_function, delta, L = 1, D = None, gamma1 = 0, gamma2 
             inv_s_thin = np.diag(1/s_thin)
             alpha_stop = 1/(delta*u.T.dot(inv_s_thin.dot(u))-1)
 
-        elif s_thin.sum() < delta: #inside
+        elif s_thin.sum() < delta: # Z inside the ball
+            
             inside += 1
             u_max, s_max, v_max = sparse.linalg.svds(grad, k = 1, which='LM')
             Zk_tilde = delta*np.outer(u_max,v_max)
             Dk = Zk - Zk_tilde
-            #BINARY SEARCH
             alpha_stop = alpha_binary_search(Zk, Dk, delta)
 
         else:
+            
             not_in_ball = True
             outside += 1
 
-        #nuclear_norm = s_thin.sum()
         # We check if Zk is inside the feasible set and the rank in order to decide which type steps to do
         if rank_Z > 1 and not_in_ball==False:
+            
+            # Change the delta at each iteration
             if delta_change is True:
-                delta = delta * (1 -0.03)
+                delta = delta * (1 + 0.03)
+            
+            # Compute Z_B and the differences with the observed values
             Z_B = Zk + alpha_stop*Dk
             diff_vec_B = np.array(Z_B[idx_rows, idx_cols] - X_rated)[0]
+            
             if 1/(objective_function(diff_vec_B)-low_bound) >= (1/(loss-low_bound)+gamma1/(2*L*D**2)):
-              # 1. Move to a lower dimensional face
+                
+                # 1. Move to a lower dimensional face
                 B_used += 1
                 Zk = Z_B
 
             else:
 
+                # Compute Beta
                 beta = compute_best_beta(Zk, X_rated, Dk, idx_rows, idx_cols, alpha_stop)
+                
+                # Use Beta to compute Z_A and the differences with the observed values
                 Z_A = Zk + beta*Dk
                 diff_vec_A = np.array(Z_A[idx_rows, idx_cols] - X_rated)[0]
 
                 if 1/(objective_function(diff_vec_A)-low_bound) >= (1/(loss-low_bound)+gamma2/(2*L*D**2)):
+                    
                     # 2. Stay in the current face
                     A_used += 1
                     Zk = Z_A
+                    
                 else:
+                    
                     # 3. Do a regular FW step and update the lower bound
                     regularFW += 1
+                    
                     #Zk update
+                    
+                    # Compute SVD
                     u_max, s_max, v_max = sparse.linalg.svds(grad, k = 1, which='LM')
+                    
+                    # Compute Dk
                     Zk_tilde = -delta*np.outer(u_max,v_max)
-
                     direction_vec = Zk_tilde.flatten() - Zk.flatten()
+
+                    # Compute new boundary
                     grad = grad.toarray()
                     wolfe_gap = grad.T.flatten() * direction_vec
                     B_w = loss + wolfe_gap.sum()
 
+                    # Take the maximum bound
                     if low_bound >= B_w:
                         new_low_bound = low_bound
                     else:
                         new_low_bound = B_w
 
-                    # Z_(k+1)
+                    # Copmute Z_(k+1)
                     alpha_k = 2/(it+2)
                     Zk = (1-alpha_k)*Zk + alpha_k*Zk_tilde
 
@@ -352,25 +409,33 @@ def FW_inface(X, objective_function, delta, L = 1, D = None, gamma1 = 0, gamma2 
 
             # 3. Do a regular FW step and update the lower bound
             not_entered += 1
-            #Zk update
+            
+            # Compute the gradient
             grad = sparse.csr_matrix((diff_vec, (idx_rows, idx_cols)))
+            
+            # Compute SVD
             u_max, s_max, v_max = sparse.linalg.svds(grad, k = 1, which='LM')
+            
+            # Compute Dk
             Zk_tilde = -delta*np.outer(u_max,v_max)
-
             direction_vec = Zk_tilde.flatten() - Zk.flatten()
+            
+            # Compute new boundary
             grad = grad.toarray()
             wolfe_gap = grad.T.flatten() * direction_vec
             B_w = loss + wolfe_gap.sum()
 
+            # Take the maximum bound
             if low_bound >= B_w:
                 new_low_bound = low_bound
             else:
                 new_low_bound = B_w
 
-            # Z_(k+1)
+            # Compute Z_(k+1)
             alpha_k = 2/(it+2)
             Zk = (1-alpha_k)*Zk + alpha_k*Zk_tilde
 
+            # Reset the sentinel back to the default value
             not_in_ball=False
 
         # Loss
@@ -386,12 +451,15 @@ def FW_inface(X, objective_function, delta, L = 1, D = None, gamma1 = 0, gamma2 
         # Count iteration
         it += 1
 
+        # Append results
         res_list.append(loss)
         ranks.append(rank_Z)
+        
         if printing == True:
-          if it % 10 == 0 or it == 1:
-            print('Iteration:', it, 'Loss:', loss, 'Loss diff:', diff_loss, 'Rank(Z): ', rank_Z)
+            if it % 10 == 0 or it == 1:
+                print('Iteration:', it, 'Loss:', loss, 'Loss diff:', diff_loss, 'Rank(Z): ', rank_Z)
 
+    # Print all the counters at the end of the algorithm
     print('Iteration:', it, 'Loss:', loss, 'Loss diff:', diff_loss, 'Rank(Z): ', rank_Z)
     print('Went to lower dim face:', B_used)
     print('Stayed:', A_used)
